1375fb5d16eccef4d2941fcf11322a40a54b7aa5
Black spy <96870884+Zdx1000@users.noreply.github.com>
2022-07-04 20:38:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,15 @@
 
 import discord
 from discord_components import ComponentsBot, Button, ButtonStyle
+import secreto_token
 
-#Definindo Prefixo | Removendo comando help
+# Definindo Prefixo | Removendo comando help | TOKEN
 
 bot = CoponentsBot(command_prefix="!")
 bot.remove_command("help")
+TOKEN = secreto_token.seu_token()
 
-#Evento Bot on
+# Evento Bot on
 
 @bot.event
 async def on_ready():
@@ -18,7 +20,7 @@ async def on_ready():
                              url=str("https://www.twitch.tv/alanzoka"))) #Transmitindo a live do alanzoka
     print(f"Olá eu sou {bot.user.name} id:{bot.user.id}, Estou on")
 
-#Comando de registro
+# Comando de registro
     
 @bot.command()
 async def registrar(ctx):
@@ -33,12 +35,15 @@ async def registrar(ctx):
   embed.set_footer(icon_url="https://cdn.discordapp.com/attachments/945064310734987264/991243063370449027/images.png",
                   text="Faça seu registro e receba seus cargos!")
   
+  # Button de registro
+  
   await ctx.send(embed=embed,
                 components=[Button(label="Registra-se",
                                    custom_id="button1",
                                    emoji=bot.get_emoji(992682236463812628),
                                    disabled=False,
                                    style=2)])
+  # 1° interação
   
   interacao = await bot.wait_for("button_click",
                                  check=lambda i: i.custom_id == "button1")
@@ -47,12 +52,16 @@ async def registrar(ctx):
                            description="Cargos: \n"
                                        "<@ids dos cargos>",
                            color=0x030303)
+  
+# Botton Gênero
+  
   await interacao.send(embed=registro,
                        components=[[Button(label="Masculino", custom_id="M", emnoji"♂️", style=2),
                                     Button(label="Feminino", custom_id="F", emoji"♀️", style=2),
                                     Button(label="Não-Binário", custom_id="BI", emoji=bot.get_emoji(992679376825106512)),
                                     Button(label="Avançar", custom_id="AV", emoji=bot.get_emoji(985174071559979068), style=ButtonStyle.blue, disabled=True),
                                     Button(label="Voltar", custom_id="VL", emoji=bot.get_emoji(985174071559979068), style=ButtonStyle.red, disabled=True)]])
+# 2° interação
   
   interacao2 = await bot.wait_for("button_click", check=lambda i: i.custom_id in ["M","F","BI"])
   
@@ -67,12 +76,15 @@ async def registrar(ctx):
    if interacao2.custom_id == "BI":
     role3 = ctx.author.guild.get_role(id_cargo)
     await ctx.author.add_roles(role3)
+    
+# Botton idade
    
     await interacao2.send(embed=idade, components=[[Button(label=" -18", custom_id="MN", emoji=bot.get_emoji(992689019429343272)),
                                                     Button(label=" +18", custom_id="MR", emoji=bot.get_emoji(992688991876960317)),
                                                     Button(label="Avançar", custom_id="AV", emoji=bot.get_emoji(985174071559979068),style=ButtonStyle.blue, disabled=True),
                                                     Button(label="Voltar", custom_id="VL", emoji=bot.get_emoji(985174071559979068), style=ButtonStyle.red, disabled=True)]])
-
+# 3° interação
+    
     interacao3 = await bot.wait_for("button_click", check=lambda i: i.custom_id in ["MN", "MR"])
 
     if interacao3.custom_id == "MN":
@@ -86,6 +98,9 @@ async def registrar(ctx):
     stats = discord.Embed(title="Qual é o seu status de relacionamento ?", description="Cargos:\n"
                                                                                        "<@&992704584529235978> <@&992705137653059594> <@&992705344323207290> <@&992705565266554882>",
                           color=0x030303)
+    
+    # Button Status social
+    
     await interacao3.send(embed=stats, components=[[Button(label="Solteiro (a)", custom_id="S", emoji=bot.get_emoji(992707278430031923)),
                                                     Button(label="Enrolado (a)", custom_id="E", emoji=bot.get_emoji(992709413821497385)),
                                                     Button(label="Namorando", custom_id="N", emoji=bot.get_emoji(992707982641074268)),
@@ -93,6 +108,8 @@ async def registrar(ctx):
                                                    [Button(label="Avançar", custom_id="AV", emoji=bot.get_emoji(985174071559979068),style=ButtonStyle.blue, disabled=True),
                                                     Button(label="Voltar", custom_id="VL", emoji=bot.get_emoji(985174071559979068), style=ButtonStyle.red, disabled=True)]])
 
+# 4° interação
+    
     interacao4 = await bot.wait_for("button_click", check=lambda i: i.custom_id in ["S", "E", "N", "C"])
 
     if interacao4.custom_id == "S":
@@ -115,6 +132,8 @@ async def registrar(ctx):
                                                                       "<@&992720432039997471> <@&992720567855759371> <@&992720605369614366> <@&992720665897619496> <@&992720707089866782> <@&992720761557110864>",
                            color=0x030303)
 
+# Button região
+    
     await interacao4.send(embed=regiao, components=[[Button(label="Sudeste", custom_id="SU", emoji="↘️"),
                                                      Button(label="Sul", custom_id="SL", emoji="⬇️"),
                                                      Button(label="Centro-Oeste", custom_id="CO", emoji="⏺️"),
@@ -124,6 +143,8 @@ async def registrar(ctx):
                                                      Button(label="Avançar", custom_id="AV", emoji=bot.get_emoji(985174071559979068), style=ButtonStyle.blue, disabled=True),
                                                      Button(label="Voltar", custom_id="VL", emoji=bot.get_emoji(985174071559979068), style=ButtonStyle.red, disabled=True)]])
 
+# 5° interação
+    
     interacao5 = await bot.wait_for("button_click", check=lambda i: i.custom_id in ["SU", "SL", "CO", "NO", "ND", "ES"])
 
     if interacao5.custom_id == "SU":
@@ -150,9 +171,11 @@ async def registrar(ctx):
         role15 = ctx.author.guild.get_role(992720761557110864)
         await ctx.author.add_roles(role15)
 
+# Botton finalização do registro
+        
     fim = discord.Embed(title="<a:certo_:992727485269680188> Registro feito com sucesso!", color=0x030303)
     await interacao5.send(embed=fim)
 
-    
-    
-bot.run("Token do seu bot")
+# inicialização do bot   
+ 
+bot.run(TOKEN)
